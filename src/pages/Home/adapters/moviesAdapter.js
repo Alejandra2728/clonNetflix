fe066fb905e50bcd4a6bc79171fca167db966a7f
Leonardo Troche <leo.m.troche@gmail.com>
2022-11-16 08:22:43 -0300

    [refactor]: improvements for adapters of the home page

--- src/pages/Home/adapters/moviesAdapter.js
+++ src/pages/Home/adapters/moviesAdapter.js
@@ -3,13 +3,15 @@ import { TMDB } from '../../../config/tmdb'
 export const adapter = (movies) => {
   return movies.map(movie => {
     return {
-      id: movie.id,
-      title: movie.title,
-      overview: movie.overview,
-      releaseDate: movie.release_date,
-      poster: TMDB.images.poster_low + movie.poster_path,
       backdrop: TMDB.images.backdrop_original + movie.backdrop_path,
-      rating: movie.vote_agerage
+      genres: movie.genres,
+      id: movie.id,
+      overview: movie.overview,
+      poster: TMDB.images.poster_low + movie.poster_path,
+      rating: movie.vote_agerage,
+      releaseDate: movie.release_date,
+      runtime: movie.runtime,
+      title: movie.title
     }
   })
 }
